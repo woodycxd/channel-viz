ecfe84123471e6dabc13bf7564b0c6989cf27b98
woodycxd <woodycxd@gmail.com>
2013-11-08 14:14:43 -0500

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,10 +12,10 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+	var defaultKey		= 'Zg933jfCAS5tEIiiLdDaB79sPwPC29UJCAOfqEkHlM263alV', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [2018650503!Raspberry Pi,127284!Average Temperature], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Woodys Feeds', // Replaces Xively logo in the header
+		dataDuration	= '6hours', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
