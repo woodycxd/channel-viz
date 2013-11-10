3ad40222d8b3f0c5c62d3cb5b614c068dd3223eb
woodycxd <woodycxd@gmail.com>
2013-11-10 10:14:03 -0500

    test

--- follows.js
+++ follows.js
@@ -15,8 +15,8 @@
 	var defaultKey		= 'Zg933jfCAS5tEIiiLdDaB79sPwPC29UJCAOfqEkHlM263alV', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [2018650503], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Woodys Test Feeds', // Replaces Xively logo in the header
-		dataDuration	= '6hours', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
+		dataDuration	= '12hours', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 30, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
